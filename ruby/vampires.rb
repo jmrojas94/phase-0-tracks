6108a03bcd62afe5f292127d7dca67577077d4a4
Juan Rojas <jmr5150@gmail.com>
2016-07-17 23:14:22 -0500

    adds conditions to age, garlic bread tolerance, and insurance.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -5,12 +5,30 @@ puts "How old are you?"
 age = gets.chomp.to_i
 
 puts "What year were you born?"
-birth_year = gets.chomp.to_i
+year = gets.chomp.to_i
+
+real_age = 2016 - year
+
+if real_age == age += 1
+	correct_age = true
+else 
+	correct_age = false
+end
 
 puts "Our company cafeteria serves garlic bread. Should we order some for you? (yes/no)"
 wants_garlic_bread = gets.chomp.downcase
 
+if wants_garlic_bread == "yes"
+	garlic_tolerance = true
+else 
+	garlic_tolerance = false
+end
+
 puts "Would you like to enroll in the company's health insurance? (yes/no)"
 wants_insurance = gets.chomp.downcase
 
-puts "#{name} #{age} #{birth_year} #{wants_insurance} #{wants_garlic_bread}"
+if wants_insurance == "yes"
+	insurance = true
+else
+	insurance = false
+end
