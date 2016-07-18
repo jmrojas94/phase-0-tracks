ef00af1c55c1c1457d37fd80ac26b99322cacfde
Juan Rojas <jmr5150@gmail.com>
2016-07-18 02:33:58 -0500

    adds loop to repeat the process for the amount of times given

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,34 +1,58 @@
-puts "What is your name?"
-name = gets.chomp
+puts "How many employees will be processed?"
+employees = gets.chomp.to_i
 
-puts "How old are you?"
-age = gets.chomp.to_i
+index = 0
 
-puts "What year were you born?"
-year = gets.chomp.to_i
+while index < employees
 
-real_age = 2016 - year
+	puts "What is your name?"
+	name = gets.chomp
 
-if real_age == age += 1
-	correct_age = true
-else 
-	correct_age = false
+	puts "How old are you?"
+	age = gets.chomp.to_i
+
+	puts "What year were you born?"
+	year = gets.chomp.to_i
+
+	real_age = 2016 - year
+
+	if real_age == age += 1
+		correct_age = true
+	else 
+		correct_age = false
+	end
+
+	puts "Our company cafeteria serves garlic bread. Should we order some for you? (yes/no)"
+	wants_garlic_bread = gets.chomp.downcase
+
+	if wants_garlic_bread == "yes"
+		garlic_tolerance = true
+	else 
+		garlic_tolerance = false
+	end
+
+	puts "Would you like to enroll in the company's health insurance? (yes/no)"
+	wants_insurance = gets.chomp.downcase
+
+	if wants_insurance == "yes"
+		insurance = true
+	else
+		insurance = false
+	end
+
+	if correct_age && (garlic_tolerance || insurance)
+		puts "Probably not a vampire."
+	elsif !correct_age && (garlic_tolerance || insurance)
+		puts "Probably a vampire."
+	elsif !correct_age && !(garlic_tolerance || insurance)
+		puts "Almost certainly a vampire."
+	elsif name == "Drake Cula" || name == "Tu Fang"
+		puts "Definitely a vampire."
+	else 
+		puts "Results inconclusive."
+	end
+
+	index +=1
+
 end
-
-puts "Our company cafeteria serves garlic bread. Should we order some for you? (yes/no)"
-wants_garlic_bread = gets.chomp.downcase
-
-if wants_garlic_bread == "yes"
-	garlic_tolerance = true
-else 
-	garlic_tolerance = false
-end
-
-puts "Would you like to enroll in the company's health insurance? (yes/no)"
-wants_insurance = gets.chomp.downcase
-
-if wants_insurance == "yes"
-	insurance = true
-else
-	insurance = false
-end
+		
