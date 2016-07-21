959a6a531892a7f73f786830c361efc0cb53a6b0
Juan Rojas <jmr5150@gmail.com>
2016-07-21 01:09:09 -0500

    edits the pseudocode and writes code for user questions and stores the data in a hash with the appropriate data type

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,10 +1,9 @@
 # Interior Design Client Form:
 # - Create a hash for client information.
+# - Ask user for the clients information.
 # 	- Store name, age, number of children, and decor theme.
 # 		- If no children,
 # 			- Return "N/A"
-# - Ask user for the clients information.
-# 	- Store users input in the appropriate key.
 # - Return the hash with all the answers.
 # - Ask the user if they want to update a key.
 # 	- If yes, 
@@ -14,3 +13,30 @@
 #   - If none,
 #       - Skip.
 # - Print the updated hash and exit program.
+
+client = {}
+
+puts "Hello! Welcome to our design client database entry system! Lets begin with a few questions."
+
+puts "What is your name?"
+client[:name] = gets.chomp
+
+puts "What is your age?"
+client[:age] = gets.chomp.to_i
+
+puts "Do you have any children? (yes/no)"
+has_children = gets.chomp.downcase
+
+if has_children == "yes"
+	client[:has_children] = true
+	puts "How many?"
+	client[:number_of_children] = gets.chomp.to_i
+else
+	client[:has_children] = false
+	client[:number_of_children] = "N/A"
+end
+
+puts "What would you like your decor theme to be?"
+client[:decor_theme] = gets.chomp
+
+puts client
