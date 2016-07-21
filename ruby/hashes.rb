7ae433765038dca799acaf6f1fa61ff4e6214e98
Juan Rojas <jmr5150@gmail.com>
2016-07-21 00:54:09 -0500

    Adds pseudocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,16 @@
+# Interior Design Client Form:
+# - Create a hash for client information.
+# 	- Store name, age, number of children, and decor theme.
+# 		- If no children,
+# 			- Return "N/A"
+# - Ask user for the clients information.
+# 	- Store users input in the appropriate key.
+# - Return the hash with all the answers.
+# - Ask the user if they want to update a key.
+# 	- If yes, 
+#		- Ask what key they would like to update.
+# 			- Update the key
+# 			- Return key and value.
+#   - If none,
+#       - Skip.
+# - Print the updated hash and exit program.
