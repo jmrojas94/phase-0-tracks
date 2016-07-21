11173b4760bf39306eb859dd79bd82ab2690d0b8
Juan Rojas <jmr5150@gmail.com>
2016-07-21 11:52:37 -0500

    writes pseudocode

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,18 @@
+# Alias Generator
+# - Define a method that takes a name as an argument.
+# - Make the name lowercase.
+# - Swap the first and last name.
+# - Split the letters in the name.
+# - Iterate through array.
+# - Find all of the vowels.
+# 	- Change vowel to the next vowel in the sequence.
+# - Find all of the consonants.
+# 	- Change consonants to the next consonant in the alphabet.
+# - Capitalize the new name.
+# - Return the new name.
+# - Ask user if a new name is wanted.
+# 	- Run alias generator until user says "quit".
+# - Store the names and aliases in a hash.
+# - For each name,
+# 	- Return "#{name} is now #{alias}."
+# - Exit program.
