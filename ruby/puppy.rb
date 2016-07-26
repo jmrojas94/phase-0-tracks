cf6c62ceb84d41c9f277853aeb2c69dde9e43b44
Juan Rojas <jmr5150@gmail.com>
2016-07-26 16:11:42 -0500

    writes a design for a puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+# Species--------------------
+# - Canis Lupus Familiaris
+
+# Characteristics------------
+# - Breed: varies
+# - Fur Color: varies
+# - Eye Count: 2
+# - Size: varies
+# - Playful: yes
+
+# Behavior-------------------
+# - Bark
+# - Eat
+# - Fetch
