4ba5f4a502675c3f50a1469fdafb554e985aeb07
Juan Rojas <jmr5150@gmail.com>
2016-07-30 16:51:03 -0500

    adds mixin module version of shout and creates the child and adult classes in which the module is passed through

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,14 +1,46 @@
+# module Shout
+
+# 	def self.yell_angrily(words)
+# 		words + "!!!" + " :("
+# 	end
+
+# 	def self.yelling_happily(words)
+# 		words + "!!!" + " :)"
+# 	end
+
+# end
+
+# p Shout.yelling_happily("Yes")
+# p Shout.yell_angrily("NO")
+
 module Shout
 
-	def self.yell_angrily(words)
-		words + "!!!" + " :("
+	def yell_angrily(words)
+		puts "#{words}" + "!!!" + " :("
 	end
 
-	def self.yelling_happily(words)
-		words + "!!!" + " :)"
+	def yell_happily(words)
+		puts "#{words}" + "!!!" + " :)"
 	end
 
 end
 
-p Shout.yelling_happily("Yes")
-p Shout.yell_angrily("NO")
+class Child
+
+	include Shout
+
+end
+
+class Stressed_Out_Adult
+
+	include Shout
+
+end
+
+child = Child.new
+child.yell_angrily("HUNGRY")
+child.yell_happily("Not hungry")
+
+adult = Stressed_Out_Adult.new
+adult.yell_angrily("UGH")
+child.yell_happily("FINALLY")
