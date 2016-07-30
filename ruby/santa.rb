9f100fa5b87c3232b18af76f8755d46c39218edf
Juan Rojas <jmr5150@gmail.com>
2016-07-29 19:09:17 -0500

    I updated the santa class with gender, ethnicity, age and reindeer ranking attributes, then updated the driver code to create multiple instances of the santa class

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,7 +1,11 @@
 class Santa
 
-	def initialize
+	def initialize(gender, ethnicity)
 		puts "Initializing Santa instance..."
+		@gender = gender
+		@ethnicity = ethnicity
+		@age = 0
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 	end
 
 	def speak
@@ -14,6 +18,16 @@ class Santa
 
 end
 
-santas = Santa.new
-santas.speak
-santas.eat_milk_and_cookies("chocolate chip")
+#Driver code
+# santas = Santa.new
+# santas.speak
+# santas.eat_milk_and_cookies("chocolate chip")
+
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
+
+p santas
