a5d094cdc8eae75a2b071276d029f3f9a1f6d60a
Juan Rojas <jmr5150@gmail.com>
2016-07-29 19:24:11 -0500

    I updated the santa class with getter methods for age and ethnicity and a setter method for gender, then added getter methods for reindeer ranking and gender to test with driver code.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -16,18 +16,61 @@ class Santa
 		puts "That was a good #{cookie} cookie!"
 	end
 
+	def get_mad_at(reindeer_name)
+		@reindeer_ranking.delete(reindeer_name)
+		@reindeer_ranking.push(reindeer_name)
+	end
+
+	def celebrate_birthday
+		@age += 1
+	end
+
+	#Setter Methods
+	def gender=(new_gender)
+		@gender = new_gender
+	end
+
+	#Getter Methods
+	def age
+		@age
+	end
+
+	def ethnicity
+		@ethnicity
+	end
+
+	def reindeer_ranking
+		@reindeer_ranking
+	end
+
+	def gender 
+		@gender
+	end
+
 end
 
 #Driver code
-# santas = Santa.new
+# santas = Santa.new("male", "Latino")
 # santas.speak
 # santas.eat_milk_and_cookies("chocolate chip")
+# p santas
+# santas.celebrate_birthday
+# p santas.age
+# p santas.ethnicity
+# p santas.reindeer_ranking
+# santas.get_mad_at("Vixen")
+# p santas.reindeer_ranking
+# p santas.gender
+# santas.gender = ("Not Male")
+# p santas.gender
 
-santas = []
-example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-example_genders.length.times do |i|
-  santas << Santa.new(example_genders[i], example_ethnicities[i])
-end
+# p santas
 
-p santas
+# santas = []
+# example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+# example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+# example_genders.length.times do |i|
+#   santas << Santa.new(example_genders[i], example_ethnicities[i])
+# end
+
+# p santas
