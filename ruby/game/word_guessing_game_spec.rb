4ed3c6cad7867eabfca010c70865cfac13de9df7
Juan Rojas <jmr5150@gmail.com>
2016-08-01 04:27:28 -0500

    fixes num of guesses and placeholder tests

--- ruby/game/word_guessing_game_spec.rb
+++ ruby/game/word_guessing_game_spec.rb
@@ -3,12 +3,12 @@ require_relative 'word_guessing_game'
 describe WordGame do 
 	let(:game) {WordGame.new("hello")}
 
-	it "finds the length of a word"
-		expect(@game.num_of_guesses).to eq 5
+	it "finds the length of a word" do
+		expect(game.num_of_guesses).to eq(5)
 	end
 
-	it "creates a placeholder for a word"
-		expect(@game.placeholder). to eq "- - - - -"
+	it "creates a placeholder for a word" do
+		expect(game.placeholder).to eq("- - - - - ")
 	end
 	
 end
