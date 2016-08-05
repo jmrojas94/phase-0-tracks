1b4d9f79d0e781e558331ce37b8628ad1d92a565
Juan Rojas <jmr5150@gmail.com>
2016-08-04 22:03:13 -0500

    adds a loop which runs the virus effects method through every state

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -4,18 +4,20 @@
 # We spent [#] hours on this challenge.
 
 # EXPLANATION OF require_relative
-#
+# Require relative is a method that takes in a string which is a path to a library/gem that is to be linked. Require is a method that is used to link library/gems not found within the directory.
 #
 require_relative 'state_data'
 
 class VirusPredictor
 
+  # This initialize method takes in 3 parameters which are the state, population density and population and declares them as instance variables.
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
 
+  # This method takes in the data from each of the private methods below (predicted deaths and speed of spread) and is used to call the data outside of the class.
   def virus_effects
     predicted_deaths(@population_density, @population, @state)
     speed_of_spread(@population_density, @state)
@@ -23,6 +25,7 @@ class VirusPredictor
 
   private
 
+  # This data takes in population density, population and state name and uses conditional statements to find predicted number of deaths by comparing population density with a given conditional integer and then multiplies the population by a given number to equal number of deaths, then prints out a statement with the state and number of deaths.
   def predicted_deaths(population_density, population, state)
     # predicted deaths is solely based on population density
     if @population_density >= 200
@@ -41,6 +44,7 @@ class VirusPredictor
 
   end
 
+  # This method takes in the population density and state data and uses conditional statements to find the speed of spread by adding a number to the speed based on the population density.
   def speed_of_spread(population_density, state) #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
@@ -70,17 +74,41 @@ end
  # initialize VirusPredictor for each state
 
 
-alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
-alabama.virus_effects
+# alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
+# alabama.virus_effects
 
-jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
-jersey.virus_effects
+# jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
+# jersey.virus_effects
 
-california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
-california.virus_effects
+# california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
+# california.virus_effects
 
-alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
-alaska.virus_effects
+# alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
+# alaska.virus_effects
+
+STATE_DATA.each do |state_name, state_info|
+# STATE_DATA.each do |state, population|
+  
+  # VirusPredictor.new(string, float, integer)
+  
+  
+  
+   state = VirusPredictor.new(state_name,
+      state_info[:population_density],
+      state_info[:population])
+  # p state_name
+  # p state_info
+  # p state_info[:population_density]
+  # p STATE_DATA["state"][:population_density]
+    state.virus_effects
+end
+
+
+# when initializing a new object (with the .new method) you are passing 3 parameters, which sets the state of the object
+# 1. state name, which is a string
+# 2. population density for that state, which is a float
+# 3. population for that state, which is an integer
+
 
 
 #=======================================================================
