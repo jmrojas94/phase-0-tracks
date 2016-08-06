385b10577cd994011f89d146969e05505daac477
Juan Rojas <jmr5150@gmail.com>
2016-08-06 14:08:31 -0500

    Creates the longest phrase function and creates two test arrays with driver code to test the function.

--- js/algos.js
+++ js/algos.js
@@ -8,3 +8,20 @@
 // 	- Else,
 // 		- Skip it.
 // - Print out the longest phrase. 
+
+function longestPhrase(array) {
+	currentLongestPhrase = '';
+	for (var i = 0; i < array.length; i++) {
+		if (array[i].length > currentLongestPhrase.length) {
+			currentLongestPhrase = array[i];
+		}
+	}
+	console.log(currentLongestPhrase);
+}
+
+var testArray = ["long phrase","longest phrase","longer phrase"]
+
+var testArray2 = ["lol", "lolololol", "lolol"]
+
+longestPhrase(testArray);
+longestPhrase(testArray2);
