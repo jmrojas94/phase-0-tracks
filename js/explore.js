cd0d1b7735439b2009ecebb034428bfe7804918e
Juan Rojas <jmr5150@gmail.com>
2016-08-06 00:58:44 -0500

    Adds psuedocode for reverse string method and creates a reverse method.

--- js/explore.js
+++ js/explore.js
@@ -1,3 +1,10 @@
+// Reverse Method
+// - Create a method that takes in a string.
+// - Create a new empty string variable.
+// - Loop through the string length backwards.
+// 	- Store each letter into empty string variable.
+// - Return the new string.
+
 function reverse(string){
 	newString = '';
 	for(var i = string.length -1; i >= 0; i-=1){
