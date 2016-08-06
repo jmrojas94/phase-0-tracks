491a8fec0fa49d1b619864200930188cb6b0fbeb
Juan Rojas <jmr5150@gmail.com>
2016-08-06 14:02:10 -0500

    Creates algos file and adds psuedocode for release 0's longest phrase function.

---
+++ js/algos.js
@@ -0,0 +1,10 @@
+// Release 0--------------------
+// Pseudocode
+// - Create a function that takes in an array.
+// - Initialize an empty string which will store the longest phrase.
+// - Loop through the array. 
+// 	- If the length of the string is longer than the longest phrase,
+// 		- Store the string in the longest phrase variable.
+// 	- Else,
+// 		- Skip it.
+// - Print out the longest phrase. 
