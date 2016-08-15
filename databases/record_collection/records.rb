6abba3f7c78dca966474d5c919b84deec6bedcec
Juan Rojas <jmr5150@gmail.com>
2016-08-15 04:21:56 -0500

    Adds pseudocode for record collection.

---
+++ databases/record_collection/records.rb
@@ -0,0 +1,12 @@
+# Record Collection Program
+
+# Create a program that stores the favorite records of a user including the artist, album, rating, and genre.
+
+# - Ask for the users name.
+# - Creat a table for the user.
+# 	- Add the artist name.
+# 	- Album name.
+# 	- Rating.
+# 	- Genre.
+# - Then ask if the user would like to store another, print the list, delete an entry, or update an item.
+# - When the user is done, print a list of albums and their data.
