dd24a06676e9969d7e4e8a27564cde7b8079d9a1
Juan Rojas <jmr5150@gmail.com>
2016-08-15 10:29:47 -0500

    Creates database using ruby, then creates add and delete methods.

--- databases/record_collection/records.rb
+++ databases/record_collection/records.rb
@@ -6,7 +6,36 @@
 # - Creat a table for the user.
 # 	- Add the artist name.
 # 	- Album name.
+#	- Release year.
 # 	- Rating.
 # 	- Genre.
 # - Then ask if the user would like to store another, print the list, delete an entry, or update an item.
 # - When the user is done, print a list of albums and their data.
+
+require 'sqlite3'
+database = SQLite3::Database.new("record_collection.db")
+
+# Create database.
+create_table_cmd = <<-SQL
+
+CREATE TABLE IF NOT EXISTS "(?)" [user] (
+	id INTEGER PRIMARY KEY,
+	album_name VARCHAR(255),
+	artist VARCHAR(255),
+	year INT,
+	rating INT,
+	genre INT
+)
+
+SQL
+
+# Adds album.
+def add_album(database, user, album_name, artist, year,
+	rating, genre)
+	database.execute("INSERT INTO (?) (album_name, artist, year, rating, genre) VALUES (?, ?, ?, ?, ?)", [user, album_name, artist, rating, genre])
+end
+
+# Deletes Entry.
+def delete_album(database, user)
+	database.execute("DELETE FROM (?) WHERE album=(?)", [user, item])
+end
