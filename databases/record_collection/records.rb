3afe124e57d2ff4ed2683c36482e2d690e3dc09e
Juan Rojas <jmr5150@gmail.com>
2016-08-15 10:49:30 -0500

    Creates a user, and tests adding to the table.

--- databases/record_collection/records.rb
+++ databases/record_collection/records.rb
@@ -15,27 +15,34 @@
 require 'sqlite3'
 database = SQLite3::Database.new("record_collection.db")
 
+puts "What"
+user = gets.chomp
+
 # Create database.
 create_table_cmd = <<-SQL
 
-CREATE TABLE IF NOT EXISTS "(?)" [user] (
+CREATE TABLE IF NOT EXISTS "#{user}" (
 	id INTEGER PRIMARY KEY,
 	album_name VARCHAR(255),
 	artist VARCHAR(255),
 	year INT,
 	rating INT,
-	genre INT
+	genre VARCHAR(255)
 )
 
 SQL
 
 # Adds album.
-def add_album(database, user, album_name, artist, year,
-	rating, genre)
-	database.execute("INSERT INTO (?) (album_name, artist, year, rating, genre) VALUES (?, ?, ?, ?, ?)", [user, album_name, artist, rating, genre])
-end
+# def add_album(database, user, album_name, artist, year,
+# 	rating, genre)
+# 	database.execute("INSERT INTO (?) (album_name, artist, year, rating, genre) VALUES (?, ?, ?, ?, ?)", [user, album_name, artist, rating, genre])
+# end
 
-# Deletes Entry.
-def delete_album(database, user)
-	database.execute("DELETE FROM (?) WHERE album=(?)", [user, item])
-end
+# # Deletes Entry.
+# def delete_album(database, user)
+# 	database.execute("DELETE FROM (?) WHERE album=(?)", [user, item])
+# end
+
+
+database.execute(create_table_cmd)
+database.execute("INSERT INTO #{user} (album_name, artist, year, rating, genre) VALUES ('lol', 'lolol', 1994, 10, 'metal')")
